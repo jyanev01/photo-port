4a29ea7888c0ef957cb630162f08a91ccbb27c05
jyanev01 <jordanyanev@gmail.com>
2022-01-16 15:34:47 -0500

    about

--- src/App.js
+++ src/App.js
@@ -6,24 +6,8 @@ import About from './components/about';
 
 function App() {
   return (
-    <div className="App">
-      <main>
-        <About></About>
-      </main>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <About></About>
     </div>
   );
 }
